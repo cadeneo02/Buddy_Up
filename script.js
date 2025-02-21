8a438656f53e8956e0a31f57abc07f467022e9b2
Caden <195048596+cadeneo02@users.noreply.github.com>
2025-02-20 22:57:29 -0600

    Update script.js
    adding functionality for social media icons in footer

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// Social media links open in new tab
+document.querySelectorAll('.footer-socials a').forEach(icon => {
+    icon.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default navigation
+        let url = icon.getAttribute('href');
+        if (url) {
+            window.open(url, '_blank'); // Open link
+        }
+    });
+});
