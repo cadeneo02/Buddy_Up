041e428b551fc5348c28b38b0a05d040ac3ac2b9
Caden <195048596+cadeneo02@users.noreply.github.com>
2025-02-20 23:58:54 -0600

    Update script.js
    added comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Social media links open in new tab
 document.socialSelector('.footer-socials a').forEach(icon => {
-    icon.addEventListener('click', (event) => {
+    icon.addEventListener('click', (event) => { // Adds listener to look for user's click
         event.preventDefault(); // Prevent default navigation
-        let url = icon.getAttribute('href');
+        let url = icon.getAttribute('href'); // This directs attatches the link from the href of the icon clicked to the function
         if (url) {
-            window.open(url, 'new_window'); // Open link
+            window.open(url, 'new_window'); // Opens link
         }
     });
 });
