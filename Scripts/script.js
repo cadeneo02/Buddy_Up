35ac1793d7c55222d8eb160baadbc112375320e2
Caden <195048596+cadeneo02@users.noreply.github.com>
2025-03-29 17:59:43 -0500

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -138,14 +138,14 @@ document.querySelectorAll('.popup-overlay').forEach(overlay => {
 });
 
 // Social media links open in new tab
-document.socialSelector('.footer-socials a').forEach(icon => {
-    icon.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent default navigation
-        let url = icon.getAttribute('href');
-        if (url) {
-            window.open(url, 'new_window'); // Open link
-        }
-    });
+document.querySelectorAll('.footer-socials a').forEach(icon => {
+  icon.addEventListener('click', event => {
+    event.preventDefault();
+    const url = icon.getAttribute('href');
+    if (url) {
+      window.open(url, '_blank');
+    }
+  });
 });
 
 // Navigation links are clickable
